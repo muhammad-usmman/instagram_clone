89ee9be0d330e868deea8b25695cfc29fe20c7dc
Muhammad Usman <musman9058@gmail.com>
2023-09-06 00:55:20 +0500

    feed_screen.dart

---
+++ lib/Screens/feed_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_clone/utilites/colors.dart';
+
+class FeedScreen extends StatelessWidget {
+  const FeedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        centerTitle: false,
+        title: SvgPicture.asset(
+          "assests/ic_instagram.svg",
+          color: primaryColor,
+          height: 32,
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.message_outlined))
+        ],
+      ),
+    );
+  }
+}
--- lib/utilites/global_variable.dart
+++ lib/utilites/global_variable.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/Screens/add_post_screen.dart';
+import 'package:instagram_clone/Screens/feed_screen.dart';
 
 
 const webScreenSize = 600;
 
  const homeScreenItems = [
-   Text('feed '),
+   FeedScreen(),
    Text('Search '),
    AddPostScreen(),
    Text('notif  '),
